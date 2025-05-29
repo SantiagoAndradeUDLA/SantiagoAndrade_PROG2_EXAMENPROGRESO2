a50936c486ff1f1d7617ada2dbb7c5b857d5ac48
SantiagoAndradeUDLA <santiago.andrade.bucheli@udla.edu.ec>
2025-05-29 16:40:03 -0500

    add de clases SA_Cuenta..

---
+++ src/SA_Cuenta.java
@@ -0,0 +1,28 @@
+public class SA_Cuenta {
+
+    public float saldo;
+    public int numeroDepositos;
+    public int numeroRetiros;
+    public float tasa;
+
+    public void Cuenta(float saldo, float tasa){
+        this.saldo = saldo;
+        this.tasa = tasa;
+    }
+
+    void depositar(){
+
+    }
+    void retirar(){
+
+    }
+
+}
+
+
+
+
+class SA_CuentaAhorros extends SA_Cuenta{
+
+
+}
